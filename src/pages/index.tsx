897a9f2ae75ae9bb66c2267973208b88b4bcdabd
johnlucky102 <johnnguyenglobal999@gmail.com>
2025-01-09 12:40:11 +0700

    test vercel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,31 @@
 'use client'
 import Head from 'next/head'
 import Button from '@mui/material/Button'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
+  const [users, setUsers] = useState<any[]>([])
+
+  const fetchData = async () => {
+    const res = await fetch('https://api-shop-lks2.onrender.com/api/users?limit=10&page=1&order=created%20asc', {
+      headers: {
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1YWZjM2E2MDFmYzQ4MzcwMWExMmYxYiIsInBlcm1pc3Npb25zIjpbIkFETUlOLkdSQU5URUQiXSwiaWF0IjoxNzM2MzMzMzE4LCJleHAiOjE3Mzg5MjUzMTh9.A68yObK8JEKGPq68Sw9w2O1rvE0aEKWo03x-xEI3AIU'
+      }
+    })
+    if (res.status === 401) {
+      console.error('Unauthorized')
+      return
+    }
+    const data = await res.json()
+    console.log(data.data.users)
+    setUsers([...users, ...data.data.users])
+    
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
     <>
       <Head>
@@ -12,6 +35,11 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <Button variant='contained'>Hello world</Button>
+      <ul>
+        {users.map((user: any) => (
+          <li key={user._id}>{user.email}</li>
+        ))}
+      </ul>
     </>
   )
 }
